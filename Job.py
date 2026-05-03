ff90c0d9ed739fb861518e355e01775267b131a2
shahzaibbakhtani <60569588+shahzaibbakhtani@users.noreply.github.com>
2026-05-03 16:10:49 +0400

    Add files via upload

---
+++ Job.py
@@ -0,0 +1,7 @@
+def Upgrade(base_salary , R = 0.1):
+    return base_salary + base_salary * R
+
+
+def bonus(total,salary):
+    """ It calculates the bonus based on total and base salary"""
+    return (total-salary)
---
+++ my_module.py
@@ -0,0 +1,5 @@
+Skill_list = ["Pyhton" , "Excel" , "Power BI"]
+
+def skill(A):
+    print(f"{A} is my favourite skill")
+
